3ede6b9641f889077467272b26d550d98c31ef29
stanley gomes <stan.gomes94@gmail.com>
2020-10-26 11:51:50 -0600

    added gestures and icon support in list item

--- App.js
+++ App.js
@@ -6,11 +6,21 @@ import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from './app/components/Screen';
+import Icon from './app/components/Icon';
+import ListItem from './app/components/ListItem';
 
 export default function App() {
   console.log("App Executed");
   return (
-    <MessagesScreen />
+    <Screen>
+      <ListItem
+        title="My Title"
+        subTitle="My SubTitle"
+        ImageComponent={
+          <Icon name="email" />
+        } />
+    </Screen>
   );
 }
 
