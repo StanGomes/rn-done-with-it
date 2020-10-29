39621a257711d7621a7da0355d457b07e3edb2e7
stanley gomes <stan.gomes94@gmail.com>
2020-10-28 20:16:04 -0600

    add tab navigation

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Button, Text } from 'react-native';
 import Screen from './app/components/Screen';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const Link = () => {
   //useNavigation hook to get access to navigation in child components
@@ -33,7 +35,7 @@ const TweetDetails = ({ navigation, route }) => (
 );
 
 const Stack = createStackNavigator();
-const StackNavigator = () => (
+const FeedNavigator = () => (
   <Stack.Navigator
     screenOptions={{
       headerStyle: { backgroundColor: "blue" },
@@ -53,12 +55,36 @@ const StackNavigator = () => (
   </Stack.Navigator>
 );
 
+const Account = () => <Screen><Text>Account</Text></Screen>;
+
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    tabBarOptions={{
+      activeBackgroundColor: 'tomato',
+      activeTintColor: 'white',
+      inactiveBackgroundColor: '#eee',
+      inactiveTintColor: 'black',
+    }}>
+    <Tab.Screen
+      name="Feed"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => <MaterialCommunityIcons name="home" size={size} color={color} />
+      }} />
+    <Tab.Screen
+      name="Account"
+      component={Account} />
+
+  </Tab.Navigator>
+);
+
 export default function App() {
   console.log("App Executed");
 
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
